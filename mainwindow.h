ce4f2ae5e8f0c1b37b64bc2c0b8ce837228dd7f0
raphaelguedes <raphaelguedesspinelli@hotmail.com>
2015-12-03 18:49:44 -0300

    Novos Arquivos
    o///

---
+++ mainwindow.h
@@ -0,0 +1,38 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+signals:
+    void mudar_atributosForca(int);
+    void mudar_atributosDestreza(int);
+    void mudar_atributosInteligencia(int);
+    void mudar_atributosConstituicao(int);
+    void mudar_atributosCarisma(int);
+
+private slots:
+
+    void on_comboBox_activated(int index);
+    void on_comboBox_2_activated(int index);
+
+    void on_label_20_linkActivated(const QString &link);
+
+private:
+    Ui::MainWindow *ui;
+    int racas[5];
+    int classes[5];
+    int forc, des, inte, cons, car;
+};
+
+#endif // MAINWINDOW_H
